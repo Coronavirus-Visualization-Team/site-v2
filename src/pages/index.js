2af56daa91dba178619f1a441c1ced81e397de63
Wanyu Guan <wanyu20@uw.edu>
2020-08-09 09:35:27 -0700

    Changed the Visualizations and More Projects titles and background image on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,10 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import { Helmet } from "react-helmet"
-import { jsx, Text, Divider, Box, Container, Grid, Image, useColorMode } from "theme-ui"
+import { jsx, Text, Divider, Box, Container, Grid, Image, useColorMode} from "theme-ui"
 import Tile from "../components/Projects/Tile"
 import VizTile from "../components/Visualizations/VizTile"
+import Background from '../components/Bitmap.png'
 import ReactGA from 'react-ga';
 
 const trackingId = "UA-171730199-2"; 
@@ -14,21 +15,59 @@ ReactGA.pageview('/');
 
 const IndexPage = (props) => {
   const partnersData = props.data.partners.edges;
-  const projectsData = props.data.projects.edges;
-  const vizData = props.data.breakingnews.edges;
 
   const [colorMode, setColorMode] = useColorMode();
 
+  const backgroundImageStyles = {
+        backgroundImage: `url(${Background})`,
+        position:'center',
+        maxWidth:'100%',
+        maxHeight: 'auto',
+        display:'block',
+        backgroundSize: 'cover',
+        Background: 'linear-gradient',
+        opacity: '0.8',
+        backgroundColor: 'grey',
+        backgroundRepeat: 'no-repeat'
+};
+
   return (
+    <div display='grid' css={backgroundImageStyles}>
     <Box
       sx={{
         position: "relative",
         display: "flex",
         flexDirection: "column",
-        alignItems: 'center'
+        // alignItems: 'center'
       }}
     >
       <Helmet title="CVT | Home" />
+      <Text
+        sx={{
+          fontSize: [1, 3],
+          maxWidth: ["100%", "100%"],
+          fontWeight: '600',
+          my: "15vh",
+          p: 4,
+          color: "white"
+        }}
+      >
+        Real Data. Real People.
+        <Text
+        sx={{
+          fontSize: [1, 2],
+          maxWidth: ["100%", "100%"],
+          fontWeight: '500',
+          mb: '3',
+        
+          color: "green"
+        }}
+      >
+        #WeAreInThisTogether.
+      </Text>
+      </Text>
+      
+      {/*
       <Text
         sx={{
           fontSize: [1, 3],
@@ -43,131 +82,6 @@ const IndexPage = (props) => {
         We're the <b>Coronavirus Visualization Team</b>,  a crowdsourced student network of data scientists and analysts, developers, and communicators working to better visualize and share the impacts, present and future, of COVID-19.
       </Text>
       
-      <Container
-              sx={{
-                position: "relative",
-                background: "#F8F8F8",
-                m: "8vh 0 0 0",
-                width: "100vw",
-                minWidth: "100vw"
-              }}
-
-      >
-      <Text
-        sx={{
-          width: "100%",
-          textAlign: "center",
-          mt: 4,
-          color: "primary",
-          fontSize: [3, 4],
-          background: "#F8F8F8"
-        }}
-      >
-        Projects
-      </Text>
-      </Container>
-
-      <Container
-        sx={{
-          position: "relative",
-          background: "#F8F8F8",
-          m: 0,
-          width: "100vw",
-          minWidth: "100vw",
-        }}
-      >
-        <Container
-          sx={{
-            maxWidth: "xl",
-          }}
-        >
-          <Grid width={["100%", "45%"]} gap={5} marginTop="4" marginRight="2vw" marginLeft="2vw">
-            {projectsData.slice(0,4).map((item) => {
-              const data = item.node.childMarkdownRemark.frontmatter;
-              return (
-                <Tile title={data.title} img={data.image} slug={data.slug}/>)
-            })}
-          </Grid>
-        </Container>
-      </Container>
-      <Link
-        sx={{
-          width: "100%",
-          textAlign: "center",
-          mt: '5vh',
-          mb: 2,
-          color: "primary",
-          fontSize: [1, 2],
-          textDecoration: "none"
-        }}
-        to="/projects"
-      >
-        See More &#187;
-      </Link>
-
-      <Container
-              sx={{
-                position: "relative",
-                background: "#F8F8F8",
-                m: 0,
-                width: "100vw",
-                minWidth: "100vw"
-              }}
-
-      >
-      <Text
-        sx={{
-          width: "100%",
-          textAlign: "center",
-          mt: 4,
-          color: "primary",
-          fontSize: [3, 4],
-          background: "#F8F8F8"
-        }}
-      >
-        Visualizations
-      </Text>
-      </Container>
-
-      <Container
-        sx={{
-          position: "relative",
-          background: "#F8F8F8",
-          m: 0,
-          width: "100vw",
-          minWidth: "100vw",
-        }}
-      >
-        <Container
-          sx={{
-            maxWidth: "xl",
-          }}
-        >
-          <Grid width={["100%", "45%"]} gap={5} marginTop="4" marginRight="2vw" marginLeft="2vw">
-            {vizData.slice(0,4).map((item) => {
-              const data = item.node.childMarkdownRemark.frontmatter;
-              return (
-                <VizTile name={data.name} image={data.image} author={data.author} link={data.link} linkTarget={data.linkTarget} />
-              )
-            })}
-          </Grid>
-        </Container>
-      </Container>
-      <Link
-        sx={{
-          width: "100%",
-          textAlign: "center",
-          mt: '5vh',
-          mb: 2,
-          color: "primary",
-          fontSize: [1, 2],
-          textDecoration: "none"
-        }}
-        to="/visualizations"
-      >
-        See More &#187;
-      </Link>
-
       <Text
         sx={{
           width: "100%",
@@ -179,7 +93,6 @@ const IndexPage = (props) => {
       >
         Our Partners
       </Text>
-
       <Box
         sx={{
           display: "flex",
@@ -196,7 +109,6 @@ const IndexPage = (props) => {
       >
         {partnersData.map((item) => {
           const data = item.node.childMarkdownRemark.frontmatter;
-
           return (
             <Box
               sx={{
@@ -219,14 +131,12 @@ const IndexPage = (props) => {
           )
         })}
       </Box>
-
       <Divider
         sx={{
           my: 2
         }}
       />
-
-      <Box
+    <Box
         sx={{
           display: "flex",
           flexDirection: "column",
@@ -240,7 +150,6 @@ const IndexPage = (props) => {
         >
           Join Us
         </Text>
-
         <Text
           sx={{
             color: "secondary",
@@ -251,7 +160,6 @@ const IndexPage = (props) => {
         >
           We’re looking for talented and driven individuals and organizations to join our team remotely.
         </Text>
-
         <Link
           sx={{
             color: "primary",
@@ -263,7 +171,9 @@ const IndexPage = (props) => {
           Learn More &#187;
         </Link>
       </Box>
+        */}
     </Box>
+    </div>
   )
 }
 
@@ -284,32 +194,4 @@ export const query = graphql`
         }
       }
     },
-    breakingnews: allFile(filter: {sourceInstanceName: {eq: "breakingnews"}} limit: 4) {
-      edges {
-        node {
-          childMarkdownRemark {
-            frontmatter {
-              name
-              author
-              image
-              link
-              linkTarget
-          }
-        }
-      }
-    }
-  },
-  projects: allFile(filter: {sourceInstanceName: {eq: "markdown-pages"}} limit: 4) {
-    edges {
-      node {
-        childMarkdownRemark {
-          frontmatter {
-            title
-            slug
-            image
-        }
-      }
-    }
-  }
-},
 }`
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { graphql, StaticQuery } from "gatsby"
-import { Container, jsx, Text, Grid, Box } from "theme-ui"
+import { Container, jsx, Text, Grid, Box, Card, Button, Image } from "theme-ui"
 import ProjectPicker from "../components/Projects/ProjectPicker"
 import Tile from "../components/Projects/Tile"
 import { Helmet } from "react-helmet"
@@ -14,7 +14,10 @@ ReactGA.initialize(trackingId);
 ReactGA.pageview('/projects');
 
 const Projects = ({ data }) => {
-  const { edges: posts } = data.allMarkdownRemark
+  const { edges: posts } = data.allMarkdownRemark;
+  const featured = posts.filter(({ node: post }) => post.frontmatter.featured === true);
+
+  const { edges: vizPosts } = data.allFile;
 
   return (
     <>
@@ -23,72 +26,174 @@ const Projects = ({ data }) => {
         sx={{
           width: "100%",
           maxWidth: "xl",
-          mb: ["5em", "20em"],
+          mb: ["5em", "5em"],
         }}
       >
+      <Text
+        sx={{
+          fontSize: [3, 4],
+          fontWeight: '700',
+          alignItems: 'center',
+          alignSelf: 'center',
+          mb: 3,
+          color: 'green',
+          textAlign: 'center'
+        }}
+      >
+        Our Work
+      </Text>
+      <Text
+        sx={{
+          fontSize: [0, 1],
+          fontWeight: '400',
+          alignItems: 'center',
+          alignSelf: 'center',
+          mb: 3,
+          color: 'black',
+          textAlign: 'center'
+        }}
+      >
+        Our team has created empirical data visualizations that educate the public 
+        on the pandemic's ongoing impact and display information from multiple, 
+        often overlooked angles such as climate implications, socioeconomic factors,
+        and social aspects.
+      </Text>
+
         <Container
           sx={{
             fontSize: [2, 3],
             position: "relative",
-            bg: "#168CA6",
-            maxWidth: "xl",
+            bg: "green",
+            maxWidth: "100%",
             p: 4,
-            borderRadius: "project",
-            height: ["fit-content", "500px"],
-            boxShadow: "base",
+            display: "flex",
+            flexDirection: "column",
+            mt: "50px",
+            mb: "30px !important"
           }}
         >
           <Text
             sx={{
-              mb: "1rem",
               fontWeight: "700",
+              textAlign: 'center',
             }}
           >
-            Here are some of the featured work we're doing.
+            Featured Projects
           </Text>
-          <ProjectPicker posts={posts}/>
+          
+          <Box
+            sx={{
+              width: "90%",
+              mx: "auto",
+              position: "relative",
+              mt: 4
+            }}
+          >
+            <Image
+              src={featured[0]?.node?.frontmatter?.image}
+            />
+
+            <Button
+              onClick = {() => {
+                if(featured[0]?.node?.frontmatter?.slug.length != 0){
+                    window.location.href = `/projects/${featured[0]?.node?.frontmatter?.slug}`
+                }
+              }}
+              sx={{
+                position: "absolute",
+                bottom: 4,
+                right: 3,
+                fontWeight: "700",
+                fontSize: "1rem",
+                bg: "white",
+                color: "black",
+                borderRadius: "button",
+                boxShadow: "0px 0px 10px rgba(255, 255, 255, 0.3)",
+                cursor: "pointer"
+              }}
+            >
+              Learn More
+            </Button>
+          </Box>
+
+          <Text
+            sx={{
+              mx: "auto",
+              textAlign: "center",
+              fontSize: '0.5em',
+              mt: 2
+            }}
+          >
+            Image Source: {featured[0]?.node?.frontmatter?.label}
+          </Text>
+          
+          <Text
+            sx={{
+              width: "90%",
+              mx: "auto",
+              mt: 3,
+              fontWeight: "700"
+            }}
+          >
+            {featured[0]?.node?.frontmatter?.title}
+          </Text>
+
+          <Text
+            sx={{
+              width: "90%",
+              mx: "auto",
+              mt: 2,
+              fontSize: "1rem"
+            }}
+          >
+            {featured[0]?.node?.html.replace(/<p>/g,'').replace('</p>','')}
+          </Text>
+
+          {/**<ProjectPicker posts={posts}/>
+          */}
         </Container>
       </Container>
 
+      
+      <Box sx={{width:"50%", float: 'left'}}>
+        <Text sx={ { variant: "styles.headerText", mb: 4, color: "black" } }>Visualizations</Text>
+      </Box>
+      <Box sx={{width:"50%", float: 'right'}}>
+        <Text sx={ { variant: "styles.headerText", mb: 4, color: "black" } }>More Projects</Text>
+      </Box>
+      
       <Container
         sx={{
           maxWidth: "xl",
-          position: "relative"
+          position: "relative",
+          display: "flex",
+          maxHeight: "800px"
         }}
       >
-        {posts && (
-          <Grid columns={[1, 2]} gap={[25, 50]}>
+        {vizPosts && (
+          <Box sx={{width:"50%", alignItems: 'top', float: 'left', overflowY: "scroll", overflowX: "visible", px: 3, py: 3}}>
+            {vizPosts.map(({ node: post }) => {
+              return <Tile
+              //slug={post.childMarkdownRemark.frontmatter.slug}
+              click={() => window.open(post.childMarkdownRemark.frontmatter.link, "_blank").focus()}
+              title={post.childMarkdownRemark.frontmatter.name}
+              img={post.childMarkdownRemark.frontmatter.image} >
+              </Tile>
+            })}
+          </Box>
+        )}
+
+        <Box sx={{width:"50%", alignItems: 'top', float: 'right', overflowY: "scroll", overflowX: "visible", px: 3, py: 3}}>
             {posts.map(({ node: post }) => {
               return <Tile
               slug={post.frontmatter.slug}
               title={post.frontmatter.title}
-              img={post.frontmatter.image} />
+              img={post.frontmatter.image} >
+              </Tile>
             })}
-          </Grid>
-        )}
+          </Box>
       </Container>
-
-
-      <Box
-          sx={{
-            fontSize: [2, 3],
-            display: "flex",
-            flexDirection: "column",
-            mt: 5
-          }}
-        >
-          <Text
-            sx={{
-              mb: "1rem",
-              fontWeight: "700",
-              color: "primary"
-            }}
-          >
-            Additional Project Information
-          </Text>
-        </Box>
-
-        <iframe class="airtable-embed" src="https://airtable.com/embed/shrFnhTP0wtwyTWDw?backgroundColor=blueLight" frameborder="0" onmousewheel="" width="100%" height="533" style={{background: 'transparent', border: '1px solid #ccc'}}></iframe>
+     
     </>
   )
 }
@@ -112,6 +217,7 @@ export default () => (
           edges {
             node {
               excerpt(pruneLength: 400)
+              html
               id
               frontmatter {
                 path
@@ -124,7 +230,22 @@ export default () => (
               }
             }
           }
-        }
+        },
+        allFile(filter: { sourceInstanceName: { eq: "viz" } }) {
+          edges {
+            node {
+              childMarkdownRemark {
+                frontmatter {
+                  name
+                  author
+                  image
+                  link
+                  linkTarget
+                }
+              }
+            }
+          }
+        } 
       }
     `}
     render={(data, count) => <Projects data={data} count={count} />}
